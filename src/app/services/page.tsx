381c7091576bbcb17b6c797a80e514b378eb275c
sujon-258549 <sujon258549@gmail.com>
2026-01-30 22:56:25 +0600

    feat: Implement the new services page by composing various service-related components.

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,4 +1,15 @@
 import ServicesHero from "@/components/services/ServicesHero";
+import Services from "@/components/home/Services";
+import ServicesTestimonial from "@/components/services/ServicesTestimonial";
+import ServicesPossible from "@/components/services/ServicesPossible";
+import ServicesEngineers from "@/components/services/ServicesEngineers";
+import ServicesVerticalSlider from "@/components/services/ServicesVerticalSlider";
+import ServicesBlog from "@/components/services/ServicesBlog";
+import ServicesBenefits from "@/components/services/ServicesBenefits";
+import ServicesProcess from "@/components/services/ServicesProcess";
+import ServicesPricing from "@/components/services/ServicesPricing";
+import ServicesFAQ from "@/components/services/ServicesFAQ";
+import ServicesCTA from "@/components/services/ServicesCTA";
 
 export default function ServicesPage() {
   return (
